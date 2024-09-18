c467cbb30cdc4a025c3073fd18e130670e3097a5
Leandro Pereira <leandronobper@gmail.com>
2024-09-18 13:56:37 -0300

    Add basic classes

---
+++ src/Program/Attack.cs
@@ -0,0 +1,4 @@
+﻿public class Attack
+{
+    protected float damage;
+}
---
+++ src/Program/Character.cs
@@ -0,0 +1,25 @@
+﻿public class Character
+{
+    protected float hp;
+    protected float maxHp;
+    protected float damage;
+    protected float defense;
+
+    public float Attack(Character character)
+    {
+        character.hp -= damage * (1 - character.defense / 100);
+        return character.hp;
+    }
+    
+    public bool isDead()
+    {
+        return hp <= 0;
+    }
+
+    public void Heal()
+    {
+        if (isDead()) return;
+
+        hp = maxHp;
+    }
+}
