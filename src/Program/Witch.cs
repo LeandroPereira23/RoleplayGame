4ced6a50cd6ad0460ed5a1024368e765a1604dc0
Paula Tomas <paulatomas1901@gmail.com>
2024-09-18 23:41:53 -0300

    correccion

--- src/Program/Witch.cs
+++ src/Program/Witch.cs
@@ -1,34 +1,34 @@
 namespace Program;
-public class Witch : Character
+public class Witch : Character  //Asignacion de herencia hijo : padre
 {
-    private MagicStaff magicStaff;
-    private Broom broom;
+    private MagicStaff magicStaff; //Asignacion de los Atributos
+    private Broom broom;  //Asignacion de los Atributos
 
 
-    public Witch(float hp, float maxHp, float damage, float defense ) : base(hp, maxHp, damage, defense)
+    public Witch(float hp, float maxHp, float damage, float defense ) : base(hp, maxHp, damage, defense) //Constructor
     {
     
     }
 
-    public void AddMagicStaff(MagicStaff magicStaff)
+    public void AddMagicStaff(MagicStaff magicStaff)  //Metodos
     {
         this.magicStaff = magicStaff;
         damage *=( magicStaff.DamageBoost +1);
     }
 
-    public void RemoveMagicStaff ()
+    public void RemoveMagicStaff ()  //Metodo 
     {
         damage /= (magicStaff.DamageBoost + 1);
         magicStaff = null;
     }
 
-    public void AddBroom(Broom broom)
+    public void AddBroom(Broom broom) //Metodo
     {
         this.broom = broom;
         defense += broom.Protection;
     }
 
-    public void RemoveBroom()
+    public void RemoveBroom()  //Metodo 
     {
         defense -= broom.Protection;
         broom = null;
