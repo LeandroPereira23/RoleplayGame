f25fad913410637722b93ac079df2d9f4daf5e2b
Matias Perez <matiperezgordano@gmail.com>
2024-11-10 21:21:09 -0300

    Battlefield tests added

--- test/LibraryTests/BattlefieldTest.cs
+++ test/LibraryTests/BattlefieldTest.cs
@@ -10,9 +10,16 @@ public class BattlefieldTest
     [Test]
     public void DoEncounterTest()
     {
+        Battlefield battlefield = new Battlefield();
+        List<Hero> heroes = new List<Hero>() { new Dwarve(1000,100,20), new Dwarve(1000,100,20), new Knight(1000,100,20)};
+        List<Enemy> enemies = new List<Enemy>() { new Goblin(10,1,2,5), new Goblin(10,1,2,5), new Goblin(10,1,2,5)};
         
+        battlefield.AddFightersToBattle(heroes, enemies);
+        
+        battlefield.DoEncounter();
+        
+        Assert.That(battlefield.Enemies.All(enemy => enemy.IsDead()));
     }
-    
     
     
     [Test]
@@ -26,6 +33,7 @@ public class BattlefieldTest
         
         battlefield.DoEncounter();
 
-        if (battlefield.Heroes.Any(hero => hero.Vp >= 5 && hero.Hp != 1000)) Assert.Fail();
+        if (battlefield.Enemies.Any(enemy => !enemy.IsDead() || battlefield.Heroes.Any(hero => hero.Vp >= 5 && hero.Hp != 1000))) Assert.Fail();
+        Assert.Pass();
     }
 }
