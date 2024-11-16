f56e8d6dfc657b22b6e3c5405e9ddd74da8f89c0
Matias Perez <matiperezgordano@gmail.com>
2024-11-16 17:48:35 -0300

    pre merge

--- src/Library/Battle/Battlefield.cs
+++ src/Library/Battle/Battlefield.cs
@@ -6,18 +6,65 @@ namespace Library.Battle;
 
 public class Battlefield
 {
-    public List<Enemy> Enemies { get; private set; } = new();
-    public List<Hero> Heroes { get; private set; } = new();
-    
+    public List<Character> Enemies { get; private set; } = new();
+    public List<Character> Heroes { get; private set; } = new();
+
+    private int[] initialHeroVPs;
+
     public void DoEncounter()
     {
-        
+        SetInitialHeroVPs();
+        while (Enemies.Any(enemy => !enemy.IsDead()) && Heroes.Any(hero => !hero.IsDead()))
+        {
+            TeamAttack(Enemies, Heroes);
+            TeamAttack(Heroes, Enemies);
+        }
+        TryToHealHeros();
+    }
+
+    private void TryToHealHeros()
+    {
+        for (int i = 0; i < Heroes.Count; i++)
+        {
+            if (!Heroes[i].IsDead() && Heroes[i].Vp - initialHeroVPs[i] > 4) 
+            {
+                Heroes[i].Heal();
+            }
+        }
+    }
+    private void SetInitialHeroVPs()
+    {
+        for (int i = 0; i < Heroes.Count; i++)
+        {
+            initialHeroVPs[i] = Heroes[0].Vp;
+        }
+    }
+    private void TeamAttack(List<Character> attacants, List<Character> recievers)
+    {
+        int aux = 0;
+        foreach (var attacant in attacants)
+        {
+            while (recievers.Any(reciever => !reciever.IsDead()))
+            {
+                if (recievers[aux].IsDead())
+                {
+                    if (aux == recievers.Count - 1) aux = 0;
+                    else
+                    {
+                        aux++;
+                    }
+                }
+            }
+            attacant.Attack(recievers[aux]);
+            aux++;
+        }
     }
 
     public void AddFightersToBattle(List<Hero> heroes, List<Enemy> enemies)
     {
-        Heroes = heroes;
-        Enemies = enemies;
+        Heroes.AddRange(heroes);
+        Enemies.AddRange(enemies);
+        initialHeroVPs = new int[Heroes.Count];
     }
     
 }
